ab41def5c72e50df4c64c6a139db8f615a905f44
Marco Antonio P <marcopandolfo577@gmail.com>
2020-02-13 15:11:49 -0300

    Criando navbar

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,21 @@
 Route::get('/series', 'SeriesController@index')
     ->name('listar_series');
 Route::get('/series/criar', 'SeriesController@create')
-    ->name('form_criar_serie');
-Route::post('/series/criar', 'SeriesController@store');
-Route::delete('/series/{id}', 'SeriesController@destroy');
-Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
+    ->name('form_criar_serie')
+    ->middleware('auth');
+Route::post('/series/criar', 'SeriesController@store')
+    ->middleware('auth');
+Route::delete('/series/{id}', 'SeriesController@destroy')
+    ->middleware('auth');
+Route::post('/series/{id}/editaNome', 'SeriesController@editaNome')
+    ->middleware('auth');
 
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
 
-Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')
+    ->middleware('auth');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -31,3 +36,8 @@ Route::get('/entrar', 'EntrarController@index');
 Route::post('/entrar', 'EntrarController@entrar');
 Route::get('/registrar', 'RegistroController@create');
 Route::post('/registrar', 'RegistroController@store');
+
+Route::get('/sair', function () {
+   \Illuminate\Support\Facades\Auth::logout();
+   return redirect('/entrar');
+});
