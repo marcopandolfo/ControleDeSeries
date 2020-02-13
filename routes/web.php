635b658700eee5e80c885aca33ab196e4f37ea3a
Marco Antonio P <marcopandolfo577@gmail.com>
2020-02-13 15:20:37 -0300

    Criando middleware personalizado

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,20 @@ Route::get('/series', 'SeriesController@index')
     ->name('listar_series');
 Route::get('/series/criar', 'SeriesController@create')
     ->name('form_criar_serie')
-    ->middleware('auth');
+    ->middleware('autenticador');
 Route::post('/series/criar', 'SeriesController@store')
-    ->middleware('auth');
+    ->middleware('autenticador');
 Route::delete('/series/{id}', 'SeriesController@destroy')
-    ->middleware('auth');
+    ->middleware('autenticador');
 Route::post('/series/{id}/editaNome', 'SeriesController@editaNome')
-    ->middleware('auth');
+    ->middleware('autenticador');
 
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
 
 Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')
-    ->middleware('auth');
+    ->middleware('autenticador');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
